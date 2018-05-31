b2b2bc1c4177e6aacfdded2adeb0fc724f54c3df
Nikola Obradović <obradovic.nix@gmail.com>
2018-05-31 16:10:04 +0200

    Inicijalni komit.

---
+++ javascript/myApp.js
@@ -0,0 +1,11 @@
+//zasto koristimo iife za definisanje AngularJS komponenti?
+(function(angular) {
+
+	var myController = function ($scope) {
+		$scope.message = "Hello from the controller!";
+	};
+
+    var application = angular.module("app", []);
+    application.controller("myCtrl", myController);
+
+})(angular);
